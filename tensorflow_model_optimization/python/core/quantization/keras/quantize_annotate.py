063e286c55fb2a2e312fb8da427bfd1357da070b
Jaehong Kim <kimjaehong@google.com>
2021-03-11 09:12:59 -0800

    Supports additional argument for QuantizeAnnotate.call.
    PiperOrigin-RevId: 362306811

--- tensorflow_model_optimization/python/core/quantization/keras/quantize_annotate.py
+++ tensorflow_model_optimization/python/core/quantization/keras/quantize_annotate.py
@@ -87,8 +87,10 @@ class QuantizeAnnotate(tf.keras.layers.Wrapper):
         hasattr(layer, '_batch_input_shape')):
       self._batch_input_shape = self.layer._batch_input_shape  # pylint: disable=protected-access
 
-  def call(self, inputs, training=None):
-    return self.layer.call(inputs)
+  def call(self, *args, **kwargs):
+    if 'training' in kwargs:
+      del kwargs['training']
+    return self.layer.call(*args, **kwargs)
 
   def get_config(self):
     base_config = super(QuantizeAnnotate, self).get_config()
